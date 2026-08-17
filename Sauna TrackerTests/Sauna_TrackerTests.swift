7613b2b7431922256b411bfefcfd5ea55440d39c
PhilippeSch <philippe.scheuber@me.com>
2026-08-17 20:48:59 +0200

    Initial Commit

---
+++ Sauna TrackerTests/Sauna_TrackerTests.swift
@@ -0,0 +1,18 @@
+//
+//  Sauna_TrackerTests.swift
+//  Sauna TrackerTests
+//
+//  Created by Philippe Scheuber on 17.08.2026.
+//
+
+import Testing
+
+struct Sauna_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
